9027e7486d9bdfdc8b2948a23730e030cd898eaa
clickfrancis <francis.source@gmail.com>
2022-03-02 22:19:49 -0300

    Estrutura de tela atividade Login

--- entrada_de_dados_exec/lib/pages/home/view/view_homepage.dart
+++ entrada_de_dados_exec/lib/pages/home/view/view_homepage.dart
@@ -48,6 +48,7 @@ class _HomePageExerciseState extends State<HomePageExercise> {
           controller: pageController,
           onPageChanged: onPageChanged,
           children: const [
+            Center(child: Text('HOMEPAGE')),
             Calculator(),
             Login(),
             Focused(),
--- entrada_de_dados_exec/lib/pages/login/view_login.dart
+++ entrada_de_dados_exec/lib/pages/login/view_login.dart
@@ -1,12 +1,56 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
   @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  bool obscureText = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Login'),
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        const Text('Login'),
+        const SizedBox(height: 30),
+        TextFormField(
+            decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.purple[50],
+          label: const Text('Usuário'),
+          border: const UnderlineInputBorder(),
+        )),
+        const SizedBox(height: 20),
+        TextFormField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.purple[50],
+            label: const Text('Senha'),
+            border: const UnderlineInputBorder(),
+          ).copyWith(
+            suffixIcon: IconButton(
+              onPressed: () {
+                setState(() {
+                  obscureText = !obscureText;
+                });
+              },
+              icon: obscureText
+                  ? const Icon(Icons.visibility_outlined)
+                  : const Icon(Icons.visibility_off_outlined),
+            ),
+          ),
+          obscureText: obscureText,
+        ),
+        const SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {},
+          child: const Text('Entrar'),
+        ),
+      ]),
     );
   }
 }
